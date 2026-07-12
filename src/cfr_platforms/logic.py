e96f66fc1213a2842426d38dbdd64b8744b17958
BootVirtual <m@ghencea.net>
2026-07-13 01:01:59 +0300

    Minor optimisations

--- src/cfr_platforms/logic.py
+++ src/cfr_platforms/logic.py
@@ -1,6 +1,10 @@
 from cfr_platforms import scraper, ocr
 from cfr_platforms.parsers import BucurestiNord
+from cachetools import TTLCache, cached
 
+cache = TTLCache(maxsize=32, ttl=60)
+
+@cached(cache)
 def get_station_data(station):
     img = scraper.get_station_webcam("BucurestiNord")
 
