9416d9e52d2048a31bc3f057d3877c200d012952
BootVirtual <m@ghencea.net>
2026-07-12 19:07:28 +0300

    More image processing to get better contrast + cropping some cells in preparation for OCR

--- src/cfr_platforms/main.py
+++ src/cfr_platforms/main.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import base64
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 res = requests.get("https://cfr.ro/gari/camereweb/index.php?statie=BucurestiNord")
 soup = BeautifulSoup(res.content, "html.parser")
@@ -25,6 +27,64 @@ departures = img[:, w//2:]
 
 arrivals = arrivals[60:340, :520]
 departures = departures[60:330, 40:540]
+
+_, departures = cv2.threshold(
+     departures,
+     0,
+     255,
+     cv2.THRESH_BINARY + cv2.THRESH_OTSU
+)
+
+projection = np.sum(departures, axis=1)
+
+# DEBUG - to be removed
+plt.plot(projection)
+plt.show()
+
+FIELDS = [
+    "type",
+    "number",
+    "destination",
+    "operator",
+    "time",
+    "delay",
+    "platform"
+]
+
+def create_row(row):
+    return {
+        "row": row,
+        "cells": {field: None for field in FIELDS},
+        "ocr": {field: None for field in FIELDS}
+    }
+
+data = {
+    "arrivals": [],
+    "departures": []
+}
+
+data["departures"].append(create_row(departures[24:41]))
+data["departures"].append(create_row(departures[41:57]))
+data["departures"].append(create_row(departures[57:74]))
+data["departures"].append(create_row(departures[87:103]))
+data["departures"].append(create_row(departures[104:120]))
+data["departures"].append(create_row(departures[120:137]))
+data["departures"].append(create_row(departures[149:166]))
+data["departures"].append(create_row(departures[166:183]))
+data["departures"].append(create_row(departures[183:200]))
+data["departures"].append(create_row(departures[212:228]))
+data["departures"].append(create_row(departures[228:245]))
+data["departures"].append(create_row(departures[245:261]))
+
+for row in data["departures"]:
+    row["cells"]["type"] = row["row"][:, 20:53]
+    row["cells"]["number"] = row["row"][:, 53:106]
+    row["cells"]["destination"] = row["row"][:, 106:260]
+    row["cells"]["operator"] = row["row"][:, 260:377]
+    row["cells"]["time"] = row["row"][:, 377:422]
+    row["cells"]["delay"] = row["row"][:, 422:462]
+    row["cells"]["platform"] = row["row"][:, 462:]
+
 
 cv2.imwrite("arrivals.png", arrivals)
 cv2.imwrite("departures.png", departures)
